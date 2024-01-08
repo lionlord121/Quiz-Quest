306f55e35732a631f17c876eb2d26ae8f9e0d6ed
Cody Craft <68160436+lionlord121@users.noreply.github.com>
2024-01-08 12:50:22 -0500

    async loading scren at main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,10 +7,12 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     public Toggle timerToggle;
+    private AsyncOperation asyncOperation;
 
     void Start()
     {
         timerToggle.isOn = GamePrefs.timerOn;
+        StartCoroutine(LoadScene());
     }
 
     public void ContinueGame()
@@ -29,14 +31,29 @@ public class MainMenu : MonoBehaviour
     {
         PlayerPrefs.SetInt("level", 0);
         GamePrefs.characterOneId = PlayerController.Character.Mage;
-        SceneManager.LoadScene("Question");
         GamePrefs.timerOn = false;
         GamePrefs.triviaMode = true;
+        asyncOperation.allowSceneActivation = true;
     }
 
     public void ToggleTimer(bool val)
     {
        GamePrefs.timerOn = val;
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        //Begin to load the Scene you specify
+        asyncOperation = SceneManager.LoadSceneAsync("Question");
+        //Don't let the Scene activate until you allow it to
+        asyncOperation.allowSceneActivation = false;
+        //When the load is still in progress, output the Text and progress bar
+        //while (!asyncOperation.isDone)
+        //{
+        //    yield return null;
+        //}
     }
 
 }
